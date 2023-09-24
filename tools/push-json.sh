2a8b784fda4544799df47a0b1698e2e71bdce661
ethmth <ethan@smaze.net>
2023-09-24 16:54:38 -0400

    push-json script for data

--- tools/push-json.sh
+++ tools/push-json.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+file="data.json"
+
+source .env
+
+if [ "$PANTRY_ID" == "" ]; then
+    echo "No Pantry information found."
+    exit 1
+fi
+
+curl -XPOST -H "Content-type: application/json" -d @$file "https://getpantry.cloud/apiv1/pantry/$PANTRY_ID/basket/$PANTRY_BASKET"
