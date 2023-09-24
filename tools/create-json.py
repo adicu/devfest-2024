886e855d9efa58ad8c30627cdc1b098a5b9141c0
ethmth <ethan@smaze.net>
2023-09-24 16:48:29 -0400

    Finished create-json script

--- tools/create-json.py
+++ tools/create-json.py
@@ -5,49 +5,50 @@ import json
 import hashlib
 import sys
 
-
 MOD = 643847
 
-workshop_fields = ["Date", "Start Time", "End Time", "Title", "Topic", "Event description", "Irl?", "Room (capacity)"]
-workshop_key_field = "Speaker email(s)"
-workshops_df = pd.read_csv("Workshops.csv")
-
-workshop_fields.append(workshop_key_field)
-workshops_key_df = workshops_df[[workshop_key_field]]
-workshops_df = workshops_df[workshop_fields]
-
-def hash_ids(ids):
-    if pd.isna(ids):
-        return []
-    id_list = [i.strip() for i in ids.split(',')]
-    # hashed_ids = [hashlib.sha256(i.strip().encode()).hexdigest() for i in id_list]
-    hashed_ids = [hash(i) % MOD for i in id_list]
-    return hashed_ids
-
-workshops_key_df["Speaker_Ids"] = workshops_key_df[workshop_key_field].apply(hash_ids)
-workshops_key_df.drop(workshop_key_field, axis=1)
-print(workshops_key_df)
-print(workshops_df)
-
-# workshops_df = pd.concat([workshops_df, workshops_key_df], axis=0, ignore_index=True)
-
-merged_df = pd.merge(workshops_df, workshops_key_df, on=workshop_key_field, how='inner')
-
-print(merged_df)
-# print(workshops_df)
-# print(workshops_key_df)
-sys.exit(1)
-workshops_data = workshops_df.to_dict(orient="records")
-
-# instructors_df = pd.read_csv("Instructors.csv")
-# instructors_data = instructors_df.to_dict(orient="records")
-
-output_data = {
-    "Workshops": workshops_data,
-    # "Instructors": instructors_data
+files = {
+    "Workshops.csv": ("Workshops", "Speaker_Ids", False, "Speaker email(s)", ["Date", "Start Time", "End Time", "Title", "Topic", "Event description", "Room (capacity)"]),
+    "Instructors.csv": ("Instructors", "Speaker_Id", True, "Speaker Email", ["Speaker name", "Bio", "Headshot (public URL preferred)", "LinkedIn", "Website"]),
+    "Events.csv": ("Events", "Speaker_Ids", False, "Speaker email(s)", ["Date", "Start Time", "End Time", "Title", "Topic", "Event description", "Room (capacity)"]),
+    "Speakers.csv": ("Speakers", "Speaker_Id", True, "Speaker Email", ["Speaker name", "Bio", "Headshot (public URL preferred)", "LinkedIn", "Website"])
 }
 
-with open("output.json", "w") as json_file:
+ofile = "data.json"
+
+def process_data(file_name, df_fields, key_field, one_id = False, id_field_name = "ids"):
+    df = pd.read_csv(file_name, keep_default_na=False)
+    fields = df_fields.copy()
+    fields.append(key_field)
+    df = df[fields]
+
+    def hash_ids(ids):
+        if pd.isna(ids):
+            return []
+        id_list = [i.strip() for i in ids.split(',')]
+
+        hashed_ids = [hash(i) % MOD for i in id_list]
+        if one_id and len(hashed_ids) > 0:
+            return hashed_ids[0]
+        return hashed_ids
+
+    df[id_field_name] = df[key_field].apply(hash_ids)
+    new_fields = df_fields.copy()
+    new_fields.append(id_field_name)
+
+    df = df[new_fields]
+
+
+    data = df.to_dict(orient="records")
+
+    return data
+
+output_data = {}
+for f in files:
+    data = process_data(f, files[f][4], files[f][3], files[f][2], files[f][1])
+    output_data[files[f][0]] = data
+
+with open(ofile, "w") as json_file:
     json.dump(output_data, json_file, indent=4)
 
-print("JSON file 'output.json' has been created.")
+print(f"JSON file '{ofile}' has been created.")
