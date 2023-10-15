5b6cd74807badd78cf94dcedf2e7ee6717aeda0d
ethmth <ethan@smaze.net>
2023-10-14 21:17:10 -0400

    Setup react transition group code

--- src/components/book-components/PageFlipApp.jsx
+++ src/components/book-components/PageFlipApp.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import styled from "@emotion/styled";
 
 import { pages } from "../Pages";
 
 import Page from "./Page";
+
+// import "../../styles/transitions.css";
 
 const colorBook = "brown";
 const colorSpine = "black";
@@ -25,16 +28,23 @@ const Book = styled.div`
   border-radius: 3px;
 `;
 
+// const PageDiv = styled.div`
+//   background-color: yellow;
+//   height: 100%;
+//   width: 100%;
+//   position: absolute;
+//   top: 0;
+//   left: 0;
+//   transform: perspective(1000px)
+//     rotateY(${({ visible }) => (visible ? 0 : -90)}deg);
+//   transition: transform 2s ease-in-out, z-index 0.5s 0.5s, opacity 0.5s 0.5s;
+//   opacity: ${({ visible }) => (visible ? 1 : 0)};
+//   z-index: ${({ visible }) => (visible ? 1000 : 999)};
+//   backface-visibility: hidden;
+// `;
+
 const PageDiv = styled.div`
   background-color: yellow;
-
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  opacity: ${({ visible }) => (visible ? 1 : 0)};
-  transition: opacity 0.5s ease-in-out;
 `;
 
 export const LeftPage = styled.div`
@@ -85,7 +95,7 @@ const PageFlipApp = (props) => {
         <button onClick={nextPage}>Next</button>
 
         <Book>
-          {pages.map((page, index) => {
+          {/* {pages.map((page, index) => {
             const pageNum = index + 1;
             // const pageLeft = pageNum % 2 == 1 ? true : false;
             // const visible = getVisible(pageNum);
@@ -100,7 +110,28 @@ const PageFlipApp = (props) => {
                 </RightPage>
               </PageDiv>
             );
-          })}
+          })} */}
+
+          {/* <PageDiv> */}
+
+          <TransitionGroup>
+            <CSSTransition
+              key={currentPage}
+              timeout={1000}
+              classNames="pageflip"
+            >
+              <PageDiv>
+                <LeftPage>
+                  <Page> {pages[currentPage - 1][0]}</Page>
+                </LeftPage>
+                <RightPage>
+                  <Page> {pages[currentPage - 1][1]}</Page>
+                </RightPage>
+              </PageDiv>
+            </CSSTransition>
+          </TransitionGroup>
+
+          {/* </PageDiv> */}
         </Book>
       </Container>
     </>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,7 @@
 import "@/styles/globals.css";
 
+import "../styles/transitions.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
