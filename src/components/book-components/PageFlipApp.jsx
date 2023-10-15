41ebdcf196563a565b07526b95180c1f73df1737
ethmth <ethan@smaze.net>
2023-10-14 22:33:26 -0400

    JSS based dynamic styling

--- src/components/book-components/PageFlipApp.jsx
+++ src/components/book-components/PageFlipApp.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 import styled from "@emotion/styled";
 
+import { createUseStyles } from "react-jss";
+
 import { pages } from "../Pages";
 
 import Page from "./Page";
@@ -46,6 +48,72 @@ export const RightPage = styled.div`
   left: 50%;
 `;
 
+const useStyles = createUseStyles({
+  pageleftEnter: {
+    zIndex: ({ forward }) => (forward ? 105 : 105),
+    transformOrigin: ({ forward }) =>
+      forward ? "right center" : "left center",
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(90deg)"
+        : "perspective(1200px) rotateY(90deg)",
+  },
+  pageleftEnterActive: {
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(0deg)"
+        : "perspective(1200px) rotateY(0deg)",
+    transition: "all 1000ms ease-in-out",
+    transitionDelay: ({ forward }) => (forward ? "1000ms" : "0ms"),
+  },
+  pageleftExit: {
+    zIndex: ({ forward }) => (forward ? 102 : 102),
+    transformOrigin: ({ forward }) =>
+      forward ? "left center" : "right center",
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(0deg)"
+        : "perspective(1200px) rotateY(0deg)",
+  },
+  pageleftExitActive: {
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(0deg)"
+        : "perspective(1200px) rotateY(0deg)",
+    transition: "all 1000ms ease-in-out",
+  },
+  pagerightEnter: {
+    zIndex: ({ forward }) => (forward ? 103 : 103),
+    transformOrigin: ({ forward }) => (forward ? "left center" : "left center"),
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(0deg)"
+        : "perspective(1200px) rotateY(0deg)",
+  },
+  pagerightEnterActive: {
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(0deg)"
+        : "perspective(1200px) rotateY(0deg)",
+    transition: "all 1000ms ease-in-out",
+  },
+  pagerightExit: {
+    zIndex: ({ forward }) => (forward ? 104 : 104),
+    transformOrigin: ({ forward }) => (forward ? "left center" : "left center"),
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(0deg)"
+        : "perspective(1200px) rotateY(0deg)",
+  },
+  pagerightExitActive: {
+    transform: ({ forward }) =>
+      forward
+        ? "perspective(1200px) rotateY(-90deg)"
+        : "perspective(1200px) rotateY(-90deg)",
+    transition: "all 1000ms ease-in-out",
+  },
+});
+
 const PageFlipApp = (props) => {
   // useEffect(() => {
   //   setCurrentPage(props.parentPage);
@@ -53,11 +121,17 @@ const PageFlipApp = (props) => {
 
   const [currentPage, setCurrentPage] = useState(props.previousPage);
 
+  const [forward, setForward] = useState(true);
+
+  const classes = useStyles({ forward });
+
   function nextPage() {
+    setForward(true);
     setCurrentPage(currentPage + 1);
   }
 
   function lastPage() {
+    setForward(false);
     setCurrentPage(currentPage - 1);
   }
 
@@ -72,7 +146,12 @@ const PageFlipApp = (props) => {
             <CSSTransition
               key={currentPage + pages.length}
               timeout={2000}
-              classNames="pageleft"
+              classNames={{
+                enter: classes.pageleftEnter,
+                enterActive: classes.pageleftEnterActive,
+                exit: classes.pageleftExit,
+                exitActive: classes.pageleftExitActive,
+              }}
             >
               <LeftPage>
                 <Page> {pages[currentPage - 1][0]}</Page>
@@ -82,7 +161,12 @@ const PageFlipApp = (props) => {
             <CSSTransition
               key={currentPage}
               timeout={2000}
-              classNames="pageright"
+              classNames={{
+                enter: classes.pagerightEnter,
+                enterActive: classes.pagerightEnterActive,
+                exit: classes.pagerightExit,
+                exitActive: classes.pagerightExitActive,
+              }}
             >
               <RightPage>
                 <Page> {pages[currentPage - 1][1]}</Page>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,5 @@
 import "@/styles/globals.css";
 
-import "../styles/transitions.css";
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
