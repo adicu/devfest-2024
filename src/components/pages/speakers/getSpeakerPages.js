a5ea7076a161e974054e66d0b5481abe68eb9c4c
ethmth <ethan@smaze.net>
2023-10-16 13:52:14 -0400

    Added ability to updatePage within specific pages

--- src/components/pages/speakers/getSpeakerPages.js
+++ src/components/pages/speakers/getSpeakerPages.js
@@ -1,4 +1,8 @@
 /* Returns an array of arrays of length 2 (left and right pages). */
-export default function getSpeakerPages(data) {
+export default function getSpeakerPages(
+  data,
+  updatePage = null,
+  pageDictionary = null
+) {
   return [[<div>Left Speaker Page 1</div>, <div>Right speaker page 1</div>]];
 }
