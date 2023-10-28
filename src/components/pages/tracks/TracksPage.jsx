d4feef673a15a7c4b51f91c7d49bbeebe54e419f
eden-chung <edenchung23@gmail.com>
2023-10-28 19:17:52 -0400

    add font and start adding components to track page

--- src/components/pages/tracks/TracksPage.jsx
+++ src/components/pages/tracks/TracksPage.jsx
@@ -1,4 +1,5 @@
 import styled from "@emotion/styled";
+import styles from './tracks.module.css';
 
 const HomePageLeft = (props) => {
   const Container = styled.div`
@@ -7,7 +8,26 @@ const HomePageLeft = (props) => {
     user-select: none;
   `;
 
-  return <Container>Tracks Page</Container>;
+  return (
+    <Container>
+      <div style={{display: 'flex'}}>
+        <div className={styles.tracksText}
+          style={{
+            fontSize: 50,
+            padding: 20,
+            letterSpacing: 5,
+          }}>
+            TRACKS
+          </div> 
+          <div style={{
+            border: '2px solid #000',
+          }}>
+            SOMEONE said that Rory has a mission for you...
+          </div>
+      </div>
+    </Container>
+   
+  );
 };
 
 export default HomePageLeft;
