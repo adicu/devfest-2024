cd0b7a7816481151deef1300ef33c1090728e3b2
eden-chung <edenchung23@gmail.com>
2023-10-29 16:26:14 -0400

    add politics image

--- src/components/pages/tracks/TracksPage.jsx
+++ src/components/pages/tracks/TracksPage.jsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import styles from './tracks.module.css';
 
+
+
 const HomePageLeft = (props) => {
   const Container = styled.div`
     height: 100%;
@@ -10,20 +12,25 @@ const HomePageLeft = (props) => {
 
   return (
     <Container>
-      <div style={{display: 'flex'}}>
-        <div className={styles.tracksText}
-          style={{
-            fontSize: 50,
-            padding: 20,
-            letterSpacing: 5,
-          }}>
-            TRACKS
-          </div> 
-          <div style={{
-            border: '2px solid #000',
-          }}>
-            SOMEONE said that Rory has a mission for you...
-          </div>
+      <div>
+        <div style={{display: 'flex'}}>
+          <div className={styles.tracksText}
+            style={{
+              fontSize: 50,
+              padding: 20,
+              letterSpacing: 5,
+            }}>
+              TRACKS
+            </div> 
+            <div style={{
+              border: '2px solid #000',
+            }}>
+              SOMEONE said that Rory has a mission for you...
+            </div>
+        </div>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <img src="/images/tracks/politics.svg" alt="Politics Image" />
+        </div>
       </div>
     </Container>
    
