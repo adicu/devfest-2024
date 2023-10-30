6d593b6324325c4814a09a377d98699447452d79
eden-chung <edenchung23@gmail.com>
2023-10-29 22:28:31 -0400

    add rest of images and add spacing

--- src/components/pages/tracks/TracksPage.jsx
+++ src/components/pages/tracks/TracksPage.jsx
@@ -12,28 +12,47 @@ const HomePageLeft = (props) => {
 
   return (
     <Container>
-      <div>
-        <div style={{display: 'flex'}}>
-          <div className={styles.tracksText}
-            style={{
-              fontSize: 50,
-              padding: 20,
-              letterSpacing: 5,
-            }}>
-              TRACKS
-            </div> 
-            <div style={{
-              border: '2px solid #000',
-            }}>
-              SOMEONE said that Rory has a mission for you...
-            </div>
+      <div style={{ position: 'relative' }}>
+        <div className={styles.tracksText}
+          style={{
+            fontSize: 50,
+            padding: 20,
+            letterSpacing: 5,
+            position: 'absolute',
+            top: '10px',
+          }}>
+          TRACKS
         </div>
-        <div style={{ display: 'flex', justifyContent: 'center' }}>
-          <img src="/images/tracks/politics.svg" alt="Politics Image" />
+        <div style={{
+          position: 'relative',
+          top: '50px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between', // Added this property
+          height: '100%'
+        }}>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <img src="/images/tracks/politics.svg" className={styles.trackimage} alt="Politics Image" style={{ width: "80%", maxWidth: '440px' }} />
+          </div>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <img src="/images/tracks/environment.svg" className={styles.trackimage} alt="Environment Image" style={{ width: "80%", maxWidth: '440px' }} />
+          </div>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+          }}>
+            <img src="/images/tracks/mental_health.svg" className={styles.trackimage} alt="Mental Health Image" style={{ width: "80%", maxWidth: '440px' }} />
+          </div>
         </div>
       </div>
-    </Container>
-   
+    </Container >
+
   );
 };
 
